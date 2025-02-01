8cb3a21a8826d007f37e7703a8eaa02798551a52
beznavorotov <beznavorotov@gmail.com>
2025-02-01 11:10:30 +0200

    fix burger menu

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// language-selector
 document.addEventListener("DOMContentLoaded", function () {
   const languageSelector = document.querySelector(".language-selector");
   const languageDropdown = document.getElementById("language-dropdown");
@@ -25,11 +26,11 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-function toggleMobileMenu() {
+// Burger-menu
+function toggleMobileMenu(event) {
   const mobileNav = document.getElementById("mobile-nav");
   const menuIcon = document.querySelector(".menu-icon");
-
-  const isMenuOpen = mobileNav.style.display === "block";
+  const isMenuOpen = window.getComputedStyle(mobileNav).display === "block";
 
   if (isMenuOpen) {
     mobileNav.style.display = "none";
@@ -51,6 +52,7 @@ function toggleMobileMenu() {
   }
 }
 
+//Slider  
 document.addEventListener("DOMContentLoaded", function () {
   const testimonials = document.querySelectorAll(".testimonial");
   const totalSlides = testimonials.length;
@@ -58,6 +60,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const prevBtn = document.getElementById("prevBtn");
   const nextBtn = document.getElementById("nextBtn");
+  const prevBtnmobile = document.getElementById("prevBtn-mobile");
+  const nextBtnmobile = document.getElementById("nextBtn-mobile");
+  const leftBtn = document.getElementById("leftBtn");
+  const rightBtn = document.getElementById("rightBtn");
   const testimonialContent = document.querySelector(".testimonial-content");
   const currentIndexText = document.getElementById("currentIndex");
 
@@ -86,40 +92,81 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   setInterval(function () {
-    nextBtn.click();
+    nextBtnmobile.click();
+  }, 5000);
+  nextBtnmobile.addEventListener("click", function () {
+    if (currentIndex < totalSlides - 1) {
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+    }
+    updateSlider();
+  });
+
+  prevBtnmobile.addEventListener("click", function () {
+    if (currentIndex > 0) {
+      currentIndex--;
+    } else {
+      currentIndex = totalSlides - 1;
+    }
+    updateSlider();
+  });
+
+  setInterval(function () {
+    rightBtn.click();
+  }, 5000);
+  rightBtn.addEventListener("click", function () {
+    if (currentIndex < totalSlides - 1) {
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+    }
+    updateSlider();
+  });
+
+  leftBtn.addEventListener("click", function () {
+    if (currentIndex > 0) {
+      currentIndex--;
+    } else {
+      currentIndex = totalSlides - 1;
+    }
+    updateSlider();
+  });
+
+  setInterval(function () {
+    rightBtn.click();
   }, 5000);
 });
 
 /* faq-section */
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".faq-question").forEach((question) => {
-      question.addEventListener("click", function () {
-          const faq = this.parentElement;
-          const answer = faq.querySelector(".faq-answer");
+    question.addEventListener("click", function () {
+      const faq = this.parentElement;
+      const answer = faq.querySelector(".faq-answer");
 
-          if (faq.classList.contains("open")) {
-              answer.style.maxHeight = "0px";
-              answer.style.opacity = "0";
-              faq.classList.remove("open");
-          } else {
-              document.querySelectorAll(".faq").forEach((item) => {
-                  if (item !== faq) {
-                      item.classList.remove("open");
-                      item.querySelector(".faq-answer").style.maxHeight = "0px";
-                      item.querySelector(".faq-answer").style.opacity = "0";
-                  }
-              });
-
-              answer.style.maxHeight = answer.scrollHeight + "px";
-              answer.style.opacity = "1";
-              faq.classList.add("open");
+      if (faq.classList.contains("open")) {
+        answer.style.maxHeight = "0px";
+        answer.style.opacity = "0";
+        faq.classList.remove("open");
+      } else {
+        document.querySelectorAll(".faq").forEach((item) => {
+          if (item !== faq) {
+            item.classList.remove("open");
+            item.querySelector(".faq-answer").style.maxHeight = "0px";
+            item.querySelector(".faq-answer").style.opacity = "0";
           }
-      });
+        });
+
+        answer.style.maxHeight = answer.scrollHeight + "px";
+        answer.style.opacity = "1";
+        faq.classList.add("open");
+      }
+    });
   });
 });
 
-
-/* Контейнер форми */
+/* Contact form */
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("contactForm");
   const nameInput = document.getElementById("name");
